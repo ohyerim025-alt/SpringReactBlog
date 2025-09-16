f121164bd3f8f0ac661b993c0b79365554b60307
ohyerim025-alt <ohyerim025@gmail.com>
2025-09-16 15:26:26 +0900

    mysql 연동

--- SpringReactBlog/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into ARTICLE (TITLE, CONTENT, created_at, updated_at) values ('제목1', '내용1', NOW(), NOW());
-insert into ARTICLE (TITLE, CONTENT, created_at, updated_at) values ('제목2', '내용2', NOW(), NOW());
-insert into ARTICLE (TITLE, CONTENT, created_at, updated_at) values ('제목3', '내용3', NOW(), NOW());
